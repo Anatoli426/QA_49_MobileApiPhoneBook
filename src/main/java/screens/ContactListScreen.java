1ab4dca1e3ef7a38db0c65685962e4c9d6fc6ccb
ly4sh <anatolimihaelov53@gmail.com>
2025-12-09 23:04:31 +0200

    AddNewContactScreen.java

---
+++ src/main/java/screens/ContactListScreen.java
@@ -0,0 +1,36 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+
+public class ContactListScreen extends BaseScreen{
+    public ContactListScreen(AppiumDriver driver) {
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "com.sheygam.contactapp:id/emptyTxt")
+    WebElement textNoContacts;
+
+    @AndroidFindBy(accessibility = "add")
+    WebElement btnPlus;
+
+    @AndroidFindBy(xpath = "//android.widget.Toast")
+    WebElement popUpMessage;
+
+    public boolean validateContactListScreenOpenAfterRegistration(String text, int time){
+        return textInElementPresent(textNoContacts, text, time);
+    }
+
+    public boolean btnPlusIsPresent(int time){
+        return isElementPresent(btnPlus, time);
+    }
+
+    public void clickBtnAdd() {
+        btnPlus.click();
+    }
+
+    public boolean validatePopUpMessage(String text, int time){
+        return textInElementPresent(popUpMessage,text, time);
+    }
+}
