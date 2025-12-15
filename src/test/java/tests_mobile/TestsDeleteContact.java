35b67041c480ae59cae655b01b257dbf7329bf66
ly4sh <anatolimihaelov53@gmail.com>
2025-12-15 21:26:46 +0200

    create get, add, edit contact api

--- src/test/java/tests_mobile/TestsDeleteContact.java
+++ src/test/java/tests_mobile/TestsDeleteContact.java
@@ -1,24 +1,41 @@
 package tests_mobile;
 
+import io.restassured.response.Response;
+import manager.AuthenticationController;
+import models.ContactsDto;
+import models.TokenDto;
 import models.User;
+import org.asynchttpclient.Request;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import screens.AddNewContactScreen;
 import screens.AuthenticationScreen;
 import screens.BaseScreen;
 import screens.ContactListScreen;
+import utils.BaseApi;
 import utils.android_utils.SwipeUtils;
 import utils.enums.Direction;
+
+import static manager.ContactController.getAllUserContacts;
+import static utils.BaseApi.ADD_NEW_CONTACT;
 
 public class TestsDeleteContact extends TestBase {
     AuthenticationScreen authenticationScreen;
     ContactListScreen contactListScreen;
-
+    ContactsDto contactsBeforeDelete, contactsAfterDelete;
+    TokenDto tokenDto;
 
     @BeforeMethod
     public void login() {
-        authenticationScreen = new AuthenticationScreen(driver);
         User user = new User("a@mail.ru", "Password123!");
+        tokenDto = AuthenticationController.requestRegLogin(user, BaseApi.LOGIN)
+                .as(TokenDto.class);
+        Response response = getAllUserContacts(ADD_NEW_CONTACT, tokenDto.getToken());
+        System.out.println(response.getStatusLine());
+        if(response.getStatusCode() == 200)
+            contactsBeforeDelete = response.as(ContactsDto.class);
+        authenticationScreen = new AuthenticationScreen(driver);
         authenticationScreen.typeAuthForm(user);
         authenticationScreen.clickBtnLogin();
         contactListScreen = new ContactListScreen(driver);
@@ -27,7 +44,11 @@ public class TestsDeleteContact extends TestBase {
     @Test
     public void deleteContactTest() {
         contactListScreen.deleteContactMiddle();
-        //validate RestAssured
+        int sizeBefore = contactsBeforeDelete.getContacts().size();
+        int sizeAfter =  getAllUserContacts(ADD_NEW_CONTACT, tokenDto.getToken())
+                .as(ContactsDto.class).getContacts().size();
+        System.out.println(sizeBefore +"----"+sizeAfter);
+        Assert.assertEquals(sizeAfter, sizeBefore-1);
     }
 
     @Test
